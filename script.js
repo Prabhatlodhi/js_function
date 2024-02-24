e9bc20050123ac24f203ebf954562e0f1899db4a
Prabhatlodhi <lodhi.prabhat@gmail.com>
2024-02-24 12:51:28 +0530

    js code

---
+++ script.js
@@ -0,0 +1,64 @@
+// let a = 5;
+// let b = 7;
+
+// let result = a+b;
+// console.log(result);
+
+
+// function addNumber (){
+//     console.log(5 + 7)
+// }
+
+// addNumber()
+// addNumber()
+// addNumber()
+// addNumber()
+
+// My name is .................
+
+//Parameter and arguments
+
+
+// let a = 54;
+// let b = 22;
+
+// function addNumber (num1, num2){ 
+//     return num1 + num2;
+// }
+
+// console.log(addNumber(5,6))
+// console.log(addNumber(a,b))
+
+
+// function smoothieMaker(banana, berries){
+//     console.log(banana, berries)
+//  const smoothie = `Smoothie with ${banana} banana and ${berries} berries.`
+//  return smoothie
+// }
+
+// console.log(smoothieMaker(5,2))
+
+
+// const greet = function (){
+//     console.log("Hello World");
+// };
+
+// console.log(greet)
+// console.log(greet())
+
+
+// const greet = ()=>  console.log("Hi"); 
+
+// console.log(greet());
+
+// const add = (a,b) => {
+//  console.log(a+b);
+//  console.log(a*b);
+// }
+// add(3,4)
+
+
+// IIFE
+(function () {
+    console.log("Hello");
+})();
